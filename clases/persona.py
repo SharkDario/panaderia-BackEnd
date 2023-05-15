142fe828ae4e6a153096b93460be76483587b022
Miguel Dario Coronel <116774936+SharkDario@users.noreply.github.com>
2023-05-15 01:19:38 -0300

    Update persona.py

--- clases/persona.py
+++ clases/persona.py
@@ -1,1 +1,30 @@
+from abc import ABC, abstractmethod
 
+
+class Persona(ABC):
+
+    def inicializarPerso(self, dni, nombre, cuil, domi):
+        self.dni = dni
+        self.nombre = nombre
+        self.cuil_cuit = cuil
+        self.domicilio = domi
+
+    def modificarPerso(self, valor, elec):
+        if (elec == "N"):
+            self.nombre = valor
+        elif (elec == "D"):
+            self.domicilio = valor
+
+    def obtenerPerso(self, elec):
+        if (elec == "D"):
+            return self.dni
+        elif (elec == "N"):
+            return self.nombre
+        elif (elec == "C"):
+            return self.cuil_cuit
+        elif (elec == "Do"):
+            return self.domicilio
+
+    @abstractmethod
+    def obtenerId(self):
+        pass
