85bc4bb3fcde736706984636c097c1ccaa421f46
Miguel Dario Coronel <116774936+SharkDario@users.noreply.github.com>
2023-05-15 01:45:26 -0300

    Update baseDeDatos.py

--- clases/baseDeDatos.py
+++ clases/baseDeDatos.py
@@ -1,1 +1,9 @@
-
+import mysql.connector
+class baseDeDatos:
+    @staticmethod
+    def abrir():
+      conexion = mysql.connector.connect(host="localhost",
+                                           user="root",
+                                           passwd="",
+                                           database="bd1")
+      return conexion
