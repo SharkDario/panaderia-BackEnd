f61eb885d6b774921691920b4e3e45bd89c18e5c
Miguel Dario Coronel <116774936+SharkDario@users.noreply.github.com>
2023-06-01 19:45:30 -0300

    Delete clases directory

--- clases/baseDeDatos.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-class baseDeDatos():
-    @staticmethod
-    def abrir():
-      conexion = mysql.connector.connect(host="localhost",
-                                           user="root",
-                                           passwd="",
-                                           database="bd1")
-      return conexion
