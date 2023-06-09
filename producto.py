2bae677fc00ee70dde5c7db4d0cf7b42ecce2a0f
Miguel Dario Coronel <116774936+SharkDario@users.noreply.github.com>
2023-06-09 09:05:45 -0300

    Update producto.py

--- producto.py
+++ producto.py
@@ -4,23 +4,23 @@ from datetime import datetime
 from baseDeDatos import baseDeDatos as bd
 import mysql.connector
 
-
+# Producto es una clase que hereda de la clase Articulo atributos: nombre, descrip, PU, stock, stockM, y funciones
 class Producto(Articulo):
     def __init__(self, nombre, descripcion, precioUnitario, stock, stockMinimo, new=None):
-        # stock minimo debe ser mayor que 0
+        # stock minimo debe ser mayor que 0, se llama a la inicializacion de la clase Articulo con los argumentos necesarios
         super().__init__(nombre, descripcion, precioUnitario, stock, stockMinimo)
-        # alta del producto
+        # alta del producto, si no se pasa ningun argumento en "new"
         if (new is None):
-            cone = bd.abrir()
-            datos = (nombre, descripcion, precioUnitario, stock, stockMinimo)
-            nAtrib = 5
+            cone = bd.abrir() # se abre la base de datos
+            datos = (nombre, descripcion, precioUnitario, stock, stockMinimo) # todos los datos se contienen en una tupla
+            nAtrib = 5 # se especifica la cantidad de atributos y en "nombreAtrib" los nombres como estan en la base de datos 
             nombreAtrib = "nombreProducto, descripcionProducto, precioUnitarioProducto, stockProducto, stockMinimoProducto"
-            bd.alta(cone, datos, "productos", nombreAtrib, nAtrib)
-
+            bd.alta(cone, datos, "productos", nombreAtrib, nAtrib) # se da de alta un producto en la tabla productos
+    # una funcion estatica
     @staticmethod
-    def bajaProducto(idProd):
-        cone = bd.abrir()
-        bd.baja(cone, (idProd, ), ("idProducto", ), "productos")
+    def bajaProducto(idProd): # para dar de baja a un producto se necesita del id Producto
+        cone = bd.abrir() # se abre la base de datos
+        bd.baja(cone, (idProd, ), ("idProducto", ), "productos") # se da de baja el producto en la tabla productos
 
     def fabricacion(self, idMateria, Cant):
         cone0 = bd.abrir()
